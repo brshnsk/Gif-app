422dff8ad3c6adcb74438eb26612860585cc2091
brshnsk <brshnsk@gmail.com>
2023-04-06 19:34:31 -0400

    [ADD] Hook

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,18 +1,13 @@
-import { useEffect, useState } from "react";
+
 import { GifItem } from "./GifItem";
-import { getGifs } from "../helpers/getGifs";
+import { useFetchGifs } from "../hooks/useFetchGifs";
 
 export const GifGrid = ({ category }) => {
-  const [images, setImages] = useState([]);
 
-  const getImages = async () => {
-    const newImages = await getGifs(category);
-    setImages(newImages);
-  };
+  const {images, isLoading} = useFetchGifs(category);
+  console.log(isLoading)
 
-  useEffect(() => {
-    getImages();
-  }, []);
+
 
   return (
     <>
